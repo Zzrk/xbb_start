9baeaef64b718ba4fee7033d110b2ec180d9f319
Zzrk <1252124617@qq.com>
2023-10-25 23:45:41 +0800

    feat: update equipmentQuality and hero stage

--- lib/declaration/index.dart
+++ lib/declaration/index.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 // 装备品质列表
-const equipmentQualityList = ['', '白', '绿', '蓝', '紫'];
+const equipmentQualityList = ['', '白', '绿', '蓝', '紫', '橙'];
+
+// 装备品质映射
+const equipmentQualityMap = {'白': 'white', '绿': 'green', '蓝': 'blue', '紫': 'purple', '橙': 'orange'};
 
 // 英雄星级列表
 const heroStarList = ['', '一星', '二星', '三星'];
@@ -9,11 +12,8 @@ const heroStarList = ['', '一星', '二星', '三星'];
 // 英雄类型映射
 const heroTypeMap = {'力': 'hero_str', '敏': 'hero_agi', '智': 'hero_int'};
 
-// 装备品质映射
-const equipmentQualityMap = {'白': 'white', '绿': 'green', '蓝': 'blue', '紫': 'purple'};
-
 // 英雄阶段列表
-const heroStageList = ['蓝+2', '紫', '紫+1', '紫+2', '紫+3'];
+const heroStageList = ['蓝+2', '紫', '紫+1', '紫+2', '紫+3', '紫+4'];
 
 // 英雄阶段选择列表
 final dropdownItems = heroStageList
