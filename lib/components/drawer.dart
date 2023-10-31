02552af1650ad64aeea1e570bffb6bacc29330c7
Zzrk <1252124617@qq.com>
2023-10-31 23:53:17 +0800

    feat: drawer icon

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -16,31 +16,37 @@ class GlobalDrawer extends StatelessWidget {
             child: Text('小冰冰, 启动!', style: TextStyle(color: Colors.white)),
           ),
           ListTile(
+            leading: const Icon(Icons.home),
             title: const Text('首页'),
             selected: Get.currentRoute == '/',
             onTap: () => Get.toNamed('/'),
           ),
           ListTile(
+            leading: const Icon(Icons.space_dashboard),
             title: const Text('装备图鉴'),
             selected: Get.currentRoute == '/equipment',
             onTap: () => Get.toNamed('/equipment'),
           ),
           ListTile(
+            leading: const Icon(Icons.emoji_emotions),
             title: const Text('英雄图鉴'),
             selected: Get.currentRoute == '/hero',
             onTap: () => Get.toNamed('/hero'),
           ),
           ListTile(
+            leading: const Icon(Icons.add_reaction),
             title: const Text('英雄养成'),
             selected: Get.currentRoute == '/hero_foster',
             onTap: () => Get.toNamed('/hero_foster'),
           ),
           ListTile(
+            leading: const Icon(Icons.widgets),
             title: const Text('我的装备'),
             selected: Get.currentRoute == '/my_equipment',
             onTap: () => Get.toNamed('/my_equipment'),
           ),
           ListTile(
+            leading: const Icon(Icons.auto_awesome),
             title: const Text('召唤模拟'),
             selected: Get.currentRoute == '/summon',
             onTap: () => Get.toNamed('/summon'),
