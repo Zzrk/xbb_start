ec13b61cee30b507b05614be553ced12e885a727
Zzrk <1252124617@qq.com>
2023-11-07 22:54:37 +0800

    chore: ci

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:xbb_start/main_app.dart';
 import 'package:xbb_start/utils/env.dart';
 
 void main() {
-  Get.put(BuildEnvironment.dev('http://10.0.2.2:8000'));
+  Get.put(BuildEnvironment.dev('http://122.51.216.75:8000/'));
   runApp(const MainApp());
 }
