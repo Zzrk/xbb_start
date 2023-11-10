0cc96a95358a9b958ab6798714362b8a074f2064
Zzrk <1252124617@qq.com>
2023-11-10 23:43:36 +0800

    feat: add r_upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:get/get.dart';
 import 'package:xbb_start/main_app.dart';
 import 'package:xbb_start/utils/env.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Plugin must be initialized before using
-  await FlutterDownloader.initialize(
-    debug: true, // optional: set to false to disable printing logs to console (default: true)
-    ignoreSsl: true, // option: set to false to disable working with http links (default: false)
-  );
-
   // 初始化环境变量
   Get.put(BuildEnvironment.release());
 
